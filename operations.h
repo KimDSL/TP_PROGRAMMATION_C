c262fd59141d3e0b1e44a6a26c7a53b951ca37ec
KimDSL <dinamoularissa@gmail.com>
2026-01-08 10:54:45 +0100

    Résolution du conflit merge

--- operations.h
+++ operations.h
@@ -10,3 +10,7 @@ int ajouterEtudiant(GestionEtudiants *gestion, Etudiant etudiant);
 Etudiant obtenirEtudiant(GestionEtudiants *gestion, const char *matricule);
 int selectionnerEtudiant(GestionEtudiants *gestion);
 void modifierEtudiant(GestionEtudiants *gestion, int index);
+int supprimerEtudiant(GestionEtudiants *gestion, int index);
+int rechercheDichotomique(Etudiant *liste, int nombre, const char *matricule);
+
+#endif
