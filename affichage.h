6722f8314af3b42f776b7849fdbd7fb4eaf372a4
koulsoumi <zoubiyatou@gmail.com>
2026-01-08 10:01:29 +0100

    Ajout de fonctions dans affichage.c et definition de la bibliotheque affichage.h

--- affichage.h
+++ affichage.h
@@ -1,5 +1,11 @@
-#ifndef D1114839_304D_483B_9A40_51D8A0F1E3CA
-#define D1114839_304D_483B_9A40_51D8A0F1E3CA
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
 
+#include "etudiant.h"
 
-#endif /* D1114839_304D_483B_9A40_51D8A0F1E3CA */
+void afficherMenu(GestionEtudiants *gestion);
+void afficherFormulaireInscrire(GestionEtudiants *gestion);
+void afficherEtudiantTrouve(Etudiant *etudiant);
+void afficherMenuRechercherEtudiant(GestionEtudiants *gestion);
+void afficherMenuRechercheDichotomique(GestionEtudiants *gestion);
+void afficherMenuModifierEtudiant(GestionEtudiants *gestion, int index);
